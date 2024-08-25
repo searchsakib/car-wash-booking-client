9840150c2e4de9d5bf211781d951bfecb8566645
searchsakib <searchsakib@gmail.com>
2024-08-25 16:52:31 +0600

    shadcn & redux initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+const App = () => {
+  return (
+    <div>
+      <h2>This is App component</h2>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default App;
